e3e9a6c32222a089c384d3ccc8d342992b7ac38b
Himanshu Agarwal <134074266+TechyCyb3r@users.noreply.github.com>
2025-08-06 11:27:52 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
       <div className='w-full h-full relative'>
         <Header />
-        <div className='pt-14 md:pt-16 absolute top-0 left-0 bg-[#11071F] -z-10'>
+        <div className='pt-14 md:pt-16 absolute top-0 left-0 bg-[#11071F] -z-10 w-auto'>
           <section id='hero'>
             <Hero />
           </section>
